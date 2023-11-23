33322cdfbf85ceaf2240f6b0999c54b91a2a65ef
giacomotolari <tolarigiacomo@gmail.com>
2023-11-23 15:52:53 +0100

    add error handling

--- add_person/addPersons.js
+++ add_person/addPersons.js
@@ -5,23 +5,37 @@ const lastNameInputElement = document.getElementById("person-lastName");
 const formElement = document.querySelector("form");
 
 const handleAddPerson = async () => {
-  const newPerson = {
-    firstName: firstNameInputElement.value,
-    lastName: lastNameInputElement.value,
-  };
-  const response = await fetch(PERSONS_API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newPerson),
-  });
+  let error = null;
+  let newPersonSent = null;
+  try {
+    const newPerson = {
+      firstName: firstNameInputElement.value,
+      lastName: lastNameInputElement.value,
+    };
+    const response = await fetch(PERSONS_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPerson),
+    });
 
-  const data = response.json();
-  return data;
+    newPersonSent = response.json();
+  } catch (err) {
+    console.log("err:", err);
+    error = {
+      message: err.message,
+      name: err.name,
+    };
+  } finally {
+    return {
+      error,
+      newPersonSent,
+    };
+  }
 };
 
-formElement.addEventListener("submit", (e) => {
+formElement.addEventListener("submit", async (e) => {
   e.preventDefault();
-  handleAddPerson();
+  await handleAddPerson();
 });
