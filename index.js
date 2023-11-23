8c898b529524b3ccee9a7c8e4a7d13d3b7fba229
giacomotolari <tolarigiacomo@gmail.com>
2023-11-23 14:30:06 +0100

    render persons, add error handling

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const PERSONS_API_URL = "http://localhost:3000/persons";
 
+const personsSection = document.getElementById("persons-section");
+
 async function getPersons() {
-  const response = await fetch(PERSONS_API_URL);
-  const persons = await response.json();
-  console.log('persons:',persons);
-  return persons
+  let error = null;
+  let persons = null;
+  try {
+    const response = await fetch(PERSONS_API_URL);
+    const data = await response.json();
+    persons = data;
+  } catch (err) {
+    error = {
+      message: err.message,
+      name: err.name,
+    };
+  } finally {
+    return {
+      error,
+      persons,
+    };
+  }
 }
 
-getPersons();
+const renderPersons = async () => {
+  const personsInfos = await getPersons();
+  console.log(
+    "🚀 ~ file: index.js:25 ~ renderPersons ~ persons:",
+    personsInfos
+  );
+
+  const personsList = personsInfos.persons.map((person) => {
+    return `<div class="person-container"><p>First Name: <strong>${person.firstName}</strong><p> <p>Last Name: <strong>${person.lastName}</strong></p></div>`;
+  });
+
+  const noProfiles =
+    "<div><h6>There are not profiles, please create one in the add profile page</h6></div>";
+
+  personsSection.innerHTML =
+    personsInfos.persons.length > 0 ? personsList.join("") : noProfiles;
+};
+
+renderPersons();
